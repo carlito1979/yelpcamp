229b9d3d7d515cf83e4713c6965c1f37b4ea1f1a
carlito1979 <carlitoandersson@gmail.com>
2017-07-14 09:53:16 +0000

    mongodb server update

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(flash());
 app.locals.moment = require("moment");
 
 // mongo db connection
-mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://yelpcamp_server:whoknowswhyIpickedthispassword@ds034807.mlab.com:34807/yelp_camp");
 mongoose.Promise = global.Promise;
 
 // user authentication set up
